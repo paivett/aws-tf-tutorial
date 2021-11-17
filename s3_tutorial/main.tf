4510817f9b5959764616feefbb97a54e8cd9b06c
spivetta <spivetta@onapsis.com>
2021-11-17 17:47:28 -0300

    Implement a s3 website

--- s3_tutorial/main.tf
+++ s3_tutorial/main.tf
@@ -24,6 +24,24 @@ resource "aws_s3_bucket" "a_public_bucket" {
   bucket = "${var.owner}-demo-bucket-public"
   acl    = "private"
 
+  policy = templatefile("policy.json", { bucket_name = "${var.owner}-demo-bucket-public", ip_address = "${var.my_ip_address}" })
+
+  tags = {
+    Name = "Demo public bucket"
+  }
+}
+
+resource "aws_s3_bucket" "a_website_bucket" {
+  bucket = "${var.owner}-demo-bucket-website"
+  acl    = "private"
+
+  policy = templatefile("policy.json", { bucket_name = "${var.owner}-demo-bucket-website", ip_address = "${var.my_ip_address}" })
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+
   tags = {
     Name = "Demo public bucket"
   }
@@ -38,7 +56,7 @@ resource "aws_s3_bucket_public_access_block" "all_blocked" {
   ignore_public_acls      = true
 }
 
-resource "aws_s3_bucket_public_access_block" "all_not_blocked" {
+resource "aws_s3_bucket_public_access_block" "all_not_blocked_public" {
   bucket = aws_s3_bucket.a_public_bucket.id
 
   block_public_acls       = false
@@ -47,27 +65,18 @@ resource "aws_s3_bucket_public_access_block" "all_not_blocked" {
   ignore_public_acls      = false
 }
 
-resource "aws_s3_bucket_policy" "a_public_policy" {
-  bucket = aws_s3_bucket.a_public_bucket.id
+resource "aws_s3_bucket_public_access_block" "all_not_blocked_website" {
+  bucket = aws_s3_bucket.a_website_bucket.id
 
-  policy = jsonencode({
-    "Id" : "Policy1637171740486",
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Sid" : "Stmt1637171737101",
-        "Action" : [
-          "s3:GetObject"
-        ],
-        "Effect" : "Allow",
-        "Resource" : "${aws_s3_bucket.a_public_bucket.arn}/*",
-        "Principal" : "*",
-        "Condition" : {
-          "IpAddress" : {
-            "aws:SourceIp" : "${var.my_ip_address}/32"
-          }
-        }
-      }
-    ]
-  })
+  block_public_acls       = false
+  block_public_policy     = false
+  restrict_public_buckets = false
+  ignore_public_acls      = false
+}
+
+resource "aws_s3_bucket_object" "index_html_object" {
+  key          = "index.html"
+  bucket       = aws_s3_bucket.a_website_bucket.id
+  source       = "index.html"
+  content_type = "text/html"
 }
